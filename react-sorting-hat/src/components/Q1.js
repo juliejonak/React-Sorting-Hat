c1d83fdf04f8a65c2efda73a177dcef05ebbc8d5
Julie Jonak <juliejonak10@gmail.com>
2018-09-29 01:11:22 -0500

    Added CSS styling

--- react-sorting-hat/src/components/Q1.js
+++ react-sorting-hat/src/components/Q1.js
@@ -9,16 +9,16 @@ const Q1 = (props) => {
             <h1 className="Welcome App-title">Question 1</h1>
             <p>When faced with a busy week, how do you prepare?</p>
 
-            <button className="answer-one" onClick={props.logHufflepuff} >
+            <button className="quiz-button answer-one" onClick={props.logHufflepuff} >
                 Prepare? I just handle it as it happens.
             </button>
-            <button className="answer-two" onClick={props.logRavenclaw} >
+            <button className="quiz-button answer-two" onClick={props.logRavenclaw} >
                 I make a detailed schedule.
             </button>
-            <button className="answer-three" onClick={props.logGryffindor} >
+            <button className="quiz-button answer-three" onClick={props.logGryffindor} >
                 I usually wait until last minute...but it all gets done.
             </button>
-            <button className="answer-four" onClick={props.logSlytherin} >
+            <button className="quiz-button answer-four" onClick={props.logSlytherin} >
                 I'll delegate as much as I can.
             </button>
         </div>
